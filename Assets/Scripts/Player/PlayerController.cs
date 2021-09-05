a763d10f380ce34c9a7657cd43d24641e40fb2b5
Shence <maoyuxuan129@gmail.com>
2021-09-05 12:39:11 +0800

    Add Player Controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SphereAdventure.Player
+{
+	/// <summary>
+	/// 玩家角色控制器
+	/// </summary>
+	public class PlayerController : MonoBehaviour
+	{
+		private float xInput;
+		private float yInput;
+
+		private PlayerMotor motor;
+
+        private void Start()
+        {
+			motor = GetComponent<PlayerMotor>();
+        }
+
+        private void Update()
+        {
+			xInput = Input.GetAxis("Horizontal");
+			yInput = Input.GetAxis("Vertical");
+
+			Vector3 move = transform.right * xInput + transform.forward * yInput;
+			motor.Movement(move);
+		}
+	}
+}
