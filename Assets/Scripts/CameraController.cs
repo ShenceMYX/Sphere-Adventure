6948b8f4a56197a352eb690bc85cf2c934aa66dc
Shence <maoyuxuan129@gmail.com>
2021-09-05 16:56:12 +0800

    Add Camera Follow

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SphereAdventure
+{
+	/// <summary>
+	/// 摄像机控制器	
+	/// </summary>
+	public class CameraController : MonoBehaviour
+	{
+		public Transform target;
+		public Vector3 offset;
+		public float smoothTime = 0.01f;
+		private Vector3 cameraVelocity = Vector3.zero;
+		private Camera mainCamera;
+
+        private void Awake()
+        {
+			mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+			transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref cameraVelocity, smoothTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Layout/LayoutSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SphereAdventure.Layout
+{
+	/// <summary>
+	/// 布局生成器
+	/// </summary>
+	public class LayoutSpawn : MonoBehaviour
+	{
+		public GameObject gridElement;
+
+	}
+}
